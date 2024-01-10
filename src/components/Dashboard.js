1f5bf8986b00066f3cc2f8dce40bc160ece21290
Nathan <nathanrmsmith@gmail.com>
2024-01-10 12:52:06 -0500

    Added state and proper this binding

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -27,24 +27,39 @@ const data = [
 ];
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.selectPanel = this.selectPanel.bind(this)
+  }
+
   state = {
-    loading: false
+    loading: false,
+    focused: null,
+  }
+
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
   }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
 
     if (this.state.loading) {
       return <Loading />
     }
 
-    const panels = data.map((panel) => (
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map((panel) => (
       <Panel 
         key={panel.id}
-        id={panel.id}
         lable={panel.label}
         value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
       />
     ))
 
